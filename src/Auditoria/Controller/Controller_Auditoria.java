572ebabbd30f67b50e20e864aff54460b9f53f2b
wrivas <wrivas@oppgraneles.com>
2021-09-19 23:47:53 -0500

    Actualizacion_Palero 10.0

--- src/Auditoria/Controller/Controller_Auditoria.java
+++ src/Auditoria/Controller/Controller_Auditoria.java
@@ -8,7 +8,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import javax.swing.JOptionPane;
+import javax.swing.JOptionPane; 
 
 public class Controller_Auditoria {
     private Connection conexion=null;
